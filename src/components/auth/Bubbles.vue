86fa0e58fdf18b981b175e5fdedba325d92fd02c
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-01-03 04:58:19 +0300

    add the Bubbles glitch effect to SignInForm

---
+++ src/components/auth/Bubbles.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="bubbles">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <defs>
+        <filter id="bubbles">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            :values="`1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${blur} -10`"
+            result="bubbles"
+          />
+          <feBlend in="SourceGraphic" in2="bubbles" operator="atop" />
+        </filter>
+      </defs>
+    </svg>
+    <div class="circle bubble-1"></div>
+    <div class="circle bubble-2"></div>
+    <div class="circle bubble-3"></div>
+    <div class="circle bubble-4"></div>
+    <div class="circle bubble-5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blur: {
+      default: () => 1555,
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@keyframes bubble-up {
+  to {
+    transform: translateY(-50vh);
+  }
+}
+
+.bubbles {
+  position: absolute;
+}
+
+.circle {
+  border-radius: 100%;
+  background-color: #eee;
+  height: 50px;
+  width: 50px;
+  top: 20px;
+  position: absolute;
+}
+
+.bubble-1 {
+  animation: bubble-up 1.3s ease-in forwards infinite;
+  left: 10%;
+}
+.bubble-2 {
+  animation: bubble-up 800ms 2s ease-in forwards infinite;
+  left: 30%;
+  width: 25px;
+  height: 25px;
+}
+.bubble-3 {
+  animation: bubble-up 2.3s 1s ease-in forwards infinite;
+  left: 50%;
+  width: 65px;
+  height: 65px;
+  top: 50px;
+}
+.bubble-4 {
+  animation: bubble-up 1s ease-in forwards infinite;
+  left: 60%;
+  width: 15px;
+  height: 15px;
+}
+.bubble-5 {
+  animation: bubble-up 1.7s ease-in forwards infinite;
+  left: 70%;
+  width: 35px;
+  height: 35px;
+  top: 30px;
+}
+</style>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,30 +1,61 @@
 <template>
   <div class="page">
-    <SignInForm />
+    <div class="container">
+      <Bubbles />
+      <SignInForm />
+    </div>
   </div>
 </template>
 
 <script>
 import SignInForm from "@/components/auth/SignInForm.vue";
+import Bubbles from "@/components/auth/Bubbles.vue";
 
 export default {
   components: {
     SignInForm,
+    Bubbles,
   },
-
   setup() {},
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/ui/elements.scss";
+@import "@/scss/anime.scss";
+
+@keyframes fade-in {
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+    filter: none;
+  }
+}
+
+svg {
+  height: 0px;
+  width: 0px;
+}
+
 .page {
   background: url("../assets/img/bg.svg");
   height: 100vh;
   display: grid;
-  align-content: center;
-  justify-items: start;
-  padding: 20px 150px;
-  gap: 10px;
+  place-content: center;
   box-sizing: content-box;
+  overflow: hidden;
+  filter: url("#bubbles");
+
+  .container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    .signin-form {
+      transform: translateY(20px);
+      animation: fade-in 500ms ease forwards;
+    }
+  }
 }
 </style>
