1334a0896fd4f28ce605b442928963b305d31804
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-01-03 04:52:29 +0300

    i18n: remove global injection

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -21,7 +21,6 @@ function loadLocaleMessages() {
 
 export default createI18n({
   legacy: false,
-  globalInjection: true,
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages()
